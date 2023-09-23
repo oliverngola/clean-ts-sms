4bf55b0e125accbc0a502e9204e8c18596a5b7a7
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2023-09-24 00:03:04 +0100

    chore add application helpers

---
+++ src/modules/@shared/application/helpers/http-helpers.ts
@@ -0,0 +1,17 @@
+import { ServerError } from '@/presentation/errors'
+import { HttpResponse } from '@/presentation/protocols'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
